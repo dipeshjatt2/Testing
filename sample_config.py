cfa0f4fab60db7ab5f469388da7f2d5256d300e3
dipeshjatt2 <101973614+dipeshjatt2@users.noreply.github.com>
2024-12-19 08:03:30 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 3748059
-api_hash = os.environ.get("API_HASH", "f8c9df448f3ba20a900bc2ffc8dae9d5")
-bot_token = os.environ.get("BOT_TOKEN")
-auth_users = os.environ.get("AUTH_USERS", "5591734243,1369808729")
-sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = os.environ.get("OWNER_USERS", "5591734243,1369808729")
+api_id = 22118129
+api_hash = os.environ.get("API_HASH", "43c66e3314921552d9330a4b05b18800")
+bot_token = os.environ.get("BOT_TOKEN","6530435719:AAHAWHqESgxbjZWVfaN8eM6eqXAMA_sWVxo")
+auth_users = os.environ.get("AUTH_USERS", "5203820046")
+sudo_users = [int(num) for num in auth_users.split("5203820046,5203820046")]
+osowner_users = os.environ.get("OWNER_USERS", "5203820046")
 owner_users = [int(num) for num in osowner_users.split(",")]
